a576a6e83d09bd6015ea89545e32516b684db040
Carlos Gomila <carlosgomila@paucasesnovescifp.cat>
2020-10-15 18:36:55 +0200

    Exercise 3 of 'Windows forms application'
    Sum calculator, using 3 inputs, 2 to enter the number and 1 to see the result. accompanied by the add button and clean

---
+++ Unit1FormsComponentesBasicosinput/Unit1FormsComponentesBasicos_input/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Unit1FormsComponentesBasicos_input
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.sum = new System.Windows.Forms.Button();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.Clean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operand1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operand2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(136, 39);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(100, 22);
+            this.num1.TabIndex = 3;
+            this.num1.TextChanged += new System.EventHandler(this.num1_TextChanged);
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(43, 206);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(90, 29);
+            this.sum.TabIndex = 4;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = true;
+            this.sum.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(136, 103);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(100, 22);
+            this.num2.TabIndex = 5;
+            this.num2.TextChanged += new System.EventHandler(this.num2_TextChanged);
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(136, 161);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(100, 22);
+            this.result.TabIndex = 6;
+            this.result.TextChanged += new System.EventHandler(this.result_TextChanged);
+            // 
+            // Clean
+            // 
+            this.Clean.Location = new System.Drawing.Point(146, 206);
+            this.Clean.Name = "Clean";
+            this.Clean.Size = new System.Drawing.Size(90, 29);
+            this.Clean.TabIndex = 7;
+            this.Clean.Text = "Clean";
+            this.Clean.UseVisualStyleBackColor = true;
+            this.Clean.Click += new System.EventHandler(this.Clean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 300);
+            this.Controls.Add(this.Clean);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Button Clean;
+    }
+}
+
---
+++ Unit1FormsComponentesBasicosinput/Unit1FormsComponentesBasicos_input/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit1FormsComponentesBasicos_input
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double d, f, resultado;
+            d = double.Parse(num1.Text);
+            f = double.Parse(num2.Text);
+        
+            resultado = d + f;
+
+            result.Text = resultado.ToString();
+        }
+
+        private void result_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            result.Text = "";
+            num1.Text = "";
+            num2.Text = "";
+        }
+    }
+}
