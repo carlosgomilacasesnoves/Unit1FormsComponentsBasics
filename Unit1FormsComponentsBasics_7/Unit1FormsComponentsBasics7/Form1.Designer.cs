7b29b369ffd70881ebdad52cf0a227697d5d1429
Carlos Gomila <carlosgomila@paucasesnovescifp.cat>
2020-10-15 19:44:04 +0200

    Exercise 6 of 'Windows forms application'
     we use a comboBox with 5 colors, when selecting a color, we show the chosen color in the textbox

---
+++ Unit1FormsComponentsBasics_7/Unit1FormsComponentsBasics7/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Unit1FormsComponentsBasics7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showsel = new System.Windows.Forms.TextBox();
+            this.comboColors = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // showsel
+            // 
+            this.showsel.Location = new System.Drawing.Point(27, 123);
+            this.showsel.Name = "showsel";
+            this.showsel.Size = new System.Drawing.Size(243, 22);
+            this.showsel.TabIndex = 0;
+            this.showsel.TextChanged += new System.EventHandler(this.showsel_TextChanged);
+            // 
+            // comboColors
+            // 
+            this.comboColors.FormattingEnabled = true;
+            this.comboColors.Items.AddRange(new object[] {
+            "Red",
+            "Yellow",
+            "Green",
+            "White",
+            "Cian"});
+            this.comboColors.Location = new System.Drawing.Point(27, 30);
+            this.comboColors.Name = "comboColors";
+            this.comboColors.Size = new System.Drawing.Size(243, 24);
+            this.comboColors.TabIndex = 1;
+            this.comboColors.SelectedIndexChanged += new System.EventHandler(this.comboColors_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboColors);
+            this.Controls.Add(this.showsel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox showsel;
+        private System.Windows.Forms.ComboBox comboColors;
+    }
+}
+
---
+++ Unit1FormsComponentsBasics_7/Unit1FormsComponentsBasics7/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit1FormsComponentsBasics7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void comboColors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            showsel.Text = "The choosen colour is: " +  comboColors.Text;
+        }
+
+        private void showsel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
